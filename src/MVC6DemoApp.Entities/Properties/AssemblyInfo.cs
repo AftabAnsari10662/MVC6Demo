11ca173867fb2e2877d33485a6e847bb632c694a
Aftab Ansari <aftabansari10662@gmail.com>
2016-07-29 10:01:20 +0530

    Action Modified

---
+++ src/MVC6DemoApp.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Honeywell")]
+[assembly: AssemblyProduct("MVC6DemoApp.Entities")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d973cc70-c5e6-4e09-8aa3-18500f958825")]
